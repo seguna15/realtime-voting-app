cc57bcbf607fbac1420e95109b2278316cd2605c
Adisa Oluwasegun Qasim <31170423+seguna15@users.noreply.github.com>
2023-11-06 10:07:32 +0100

    removed abort signal

--- frontend/src/hooks/useAxiosFetch.js
+++ frontend/src/hooks/useAxiosFetch.js
@@ -9,45 +9,22 @@ const useAxiosFetch = (dataUrl) => {
     const [fetchError, setFetchError] = useState(null);
 
     useEffect(() => {
-        let isMounted = true;
-        const abortController = new AbortController()
-
+        
         const fetchData = async (url) => {
             setStatus(CALL_STATUS.LOADING);
             try{
-                const response = await axios.get(url, {
-                    withCredentials: true,
-                    signal: abortController.signal
-                });
-                if(isMounted) {
-                    console.log(response.data)
-                    setData(response.data);
-                    setStatus(CALL_STATUS.SUCCESS)
-                }
-            }catch(error){
-                if(isMounted){
-                     if (axios.isCancel(error)) {
-                       setFetchError("Operation cancelled");
-                       setStatus(CALL_STATUS.ERROR);
-                     }
-                     else {
-                       setFetchError(error.response.data.message);
-                       setStatus(CALL_STATUS.ERROR);
-                     }
-                }
-            }finally{
-                isMounted = false;
+                const response = await axios.get(url, {withCredentials: true});
+                
+                console.log(response.data)
+                setData(response.data);
+                setStatus(CALL_STATUS.SUCCESS)
+            }catch(error){  
+                setFetchError(error.response.data.message);
+                setStatus(CALL_STATUS.ERROR);
             }
         }
 
         fetchData(dataUrl);
-
-        const cleanUp = () => {
-            isMounted = false;
-            abortController.abort();
-        }
-
-        return cleanUp
     }, [dataUrl]);
 
     const statusObj = {
@@ -55,7 +32,7 @@ const useAxiosFetch = (dataUrl) => {
         isSuccess: status === CALL_STATUS.SUCCESS,
         isError: status === CALL_STATUS.ERROR,
     }
-
+    
     return { data, fetchError, statusObj };
 }
 
